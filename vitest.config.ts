69b0dcf3ddd63ae38253adfc4b4dff3378279b60
operonmaster <pavel@trustoperon.com>
2026-05-28 21:11:31 -0700

    fix: include specialty rx component tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     }
   },
   test: {
-    include: ["src/**/*.test.ts"],
+    include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
     setupFiles: ["./vitest.setup.ts"],
     globals: true
   }
